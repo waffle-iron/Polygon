94908cdb3f76940a5d2f621b93d5ae5a33a5fdd5
Bruger <Bruger@10.20.146.40>
2016-03-30 10:11:01 +0200

    Hjælpeklasse til firm er lavet.

--- WebApplication1/src/java/helperClasses/Firm.java
+++ WebApplication1/src/java/helperClasses/Firm.java
@@ -5,11 +5,37 @@
  */
 package helperClasses;
 
-/**
- *
- * @author peter L Lange
- */
+
 public class Firm
 {
+    int ContactNumber;
+    String ContactMail;
+
+    public Firm(String ContactNumer, String ContactMail)
+    {
+        this.ContactNumber = Integer.parseInt(ContactNumer);
+        this.ContactMail = ContactMail;
+    }
+
+    public int getContactNumber()
+    {
+        return ContactNumber;
+    }
+
+    public String getContactMail()
+    {
+        return ContactMail;
+    }
+
+    public void setContactNumber(int ContactNumber)
+    {
+        this.ContactNumber = ContactNumber;
+    }
+
+    public void setContactMail(String ContactMail)
+    {
+        this.ContactMail = ContactMail;
+    }
+    
     
 }
