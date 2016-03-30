df0db5dc6388d05eed8f97588884fb7a40861f48
peterleolange <peterleolange@outlook.dk>
2016-03-30 09:40:28 +0200

    oprettet 3 helperclasses uden at give dem kode

---
+++ WebApplication1/src/java/helperClasses/ReportPage.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helperClasses;
+
+/**
+ *
+ * @author peter L Lange
+ */
+public class ReportPage
+{
+    
+}
