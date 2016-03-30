bc4a2d3e5c008a8bf386f6b9881438efad654d2a
peterleolange <peterleolange@outlook.dk>
2016-03-30 10:55:57 +0200

    lavet helperclassesne til reportne

--- WebApplication1/src/java/helperClasses/Comment.java
+++ WebApplication1/src/java/helperClasses/Comment.java
@@ -4,12 +4,42 @@
  * and open the template in the editor.
  */
 package helperClasses;
-
 /**
  *
  * @author peter L Lange
  */
 public class Comment
 {
+    private String text;
+    private String type;
+    public Comment(String text, String type)
+    {
+        this.text = text;
+        this.type = type;
+    }
+
+    public String getText()
+    {
+        return text;
+    }
+
+    public void setText(String text)
+    {
+        this.text = text;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public void setType(String type)
+    {
+        this.type = type;
+    }
+   
+    
+    
+    
     
 }
--- WebApplication1/src/java/helperClasses/Report.java
+++ WebApplication1/src/java/helperClasses/Report.java
@@ -11,5 +11,93 @@ package helperClasses;
  */
 public class Report
 {
-    
+    int reportnr;
+    int buildingID;
+    Date reportDate;
+    int state;
+    ReportPage[] reportPages;
+    Comment outerWalls;
+    Comment roof;
+
+    public Report(int reportnr, int buildingID, Date reportDate, int state, ReportPage[] reportPages, Comment outerWalls, Comment roof)
+    {
+        this.reportnr = reportnr;
+        this.buildingID = buildingID;
+        this.reportDate = reportDate;
+        this.state = state;
+        this.reportPages = reportPages;
+        this.outerWalls = outerWalls;
+        this.roof = roof;
+    }
+
+    public int getReportnr()
+    {
+        return reportnr;
+    }
+
+    public void setReportnr(int reportnr)
+    {
+        this.reportnr = reportnr;
+    }
+
+    public int getBuildingID()
+    {
+        return buildingID;
+    }
+
+    public void setBuildingID(int buildingID)
+    {
+        this.buildingID = buildingID;
+    }
+
+    public Date getReportDate()
+    {
+        return reportDate;
+    }
+
+    public void setReportDate(Date reportDate)
+    {
+        this.reportDate = reportDate;
+    }
+
+    public int getState()
+    {
+        return state;
+    }
+
+    public void setState(int state)
+    {
+        this.state = state;
+    }
+
+    public ReportPage[] getReportPages()
+    {
+        return reportPages;
+    }
+
+    public void setReportPages(ReportPage[] reportPages)
+    {
+        this.reportPages = reportPages;
+    }
+
+    public Comment getOuterWalls()
+    {
+        return outerWalls;
+    }
+
+    public void setOuterWalls(Comment outerWalls)
+    {
+        this.outerWalls = outerWalls;
+    }
+
+    public Comment getRoof()
+    {
+        return roof;
+    }
+
+    public void setRoof(Comment roof)
+    {
+        this.roof = roof;
+    }
+
 }
