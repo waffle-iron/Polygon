88c716f8a2c192987aa01d35aacf9834be5c4537
peterleolange <peterleolange@outlook.dk>
2016-03-31 10:37:54 +0200

    opdateret facade

--- WebApplication1/src/java/Controller/Facade.java
+++ WebApplication1/src/java/Controller/Facade.java
@@ -2,9 +2,19 @@ package Controller;
 
 import Domain.BuildingDataMapper;
 import Domain.FirmDataMapper;
+import helperClasses.*;
 
 public class Facade
 {
     BuildingDataMapper buildingDM = new BuildingDataMapper();
-    FirmDataMapper firmDM = new FirmDataMapper();   
+    FirmDataMapper firmDM = new FirmDataMapper();
+    
+    public void addBuildingToDB(Building build)
+    {
+        buildingDM.addBuildingToDB(build);
+    }
+    public void addFirmToDB(Firm firm)
+    {
+        firmDM.addFirmToDB(firm);
+    }
 }
