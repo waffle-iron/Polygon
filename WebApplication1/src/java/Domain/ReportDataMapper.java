2ac461bb9e4ec7588930150431357b66530d8971
EmilUlrikChristoffersen <emilulrikchristoffersen@outlook.dk>
2016-03-31 11:30:30 +0200

    added getReportsFromDB

--- WebApplication1/src/java/Domain/ReportDataMapper.java
+++ WebApplication1/src/java/Domain/ReportDataMapper.java
@@ -87,10 +87,10 @@ public class ReportDataMapper {
                     info[0] = res.getInt(2);
                     info[1] = res.getInt(3);
                     info[2] = res.getInt(5);
-                    arr.add(new ReportPage(res.getInt(1), res.getBoolean(6), new Date(res.getDate(7)), res.getString(8), res.getString(9), res.getString(10), res.getBoolean(11), res.getBoolean(12), res.getBoolean(13), res.getBoolean(14), res.getString(15), res.getBoolean(16), null));
+                    arr.add(new ReportPage(res.getInt(2),res.getInt(1), res.getBoolean(6), new Date(res.getDate(7)), res.getString(8), res.getString(9), res.getString(10), res.getBoolean(11), res.getBoolean(12), res.getBoolean(13), res.getBoolean(14), res.getString(15), res.getBoolean(16), null));
                 }
             }
-            report = new Report(res.getInt(2), res.getInt(3),new Date(res.getDate(4)), res.getInt(5), (ReportPage[])arr.toArray(), null, null);
+            report = new Report(res.getInt(2), res.getInt(3), new Date(res.getDate(4)), res.getInt(5), (ReportPage[]) arr.toArray(), null, null);
             {
 
             }
@@ -100,5 +100,65 @@ public class ReportDataMapper {
             throw new IllegalArgumentException("not valid reportID");
         }
         return report;
+    }
+
+    public ArrayList<Report> getReportsFromDB() {
+        ArrayList<Report> report = new ArrayList<>();
+
+        int[] info = new int[3];
+        ArrayList<ReportPage> arr = new ArrayList<>();
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(Connector.URL, Connector.USERNAME, Connector.PASSWORD);
+            Statement statement = con.createStatement();
+            ResultSet res = statement.executeQuery("select  ReportPageNr ,reportNR, (select BuildingID from report where report.reportNR  = e.reportNR),"
+                    + "(select `Date` from report where report.reportNR  = e.reportNR),(select StateNR from report where report.reportNR  = e.reportNR), PreviousDamaged, Damagedate,DamagedPlace,Cause,Repairs,Moist,Rot,Mold,fire,other,MoistScan from reportpage e;");
+            res.beforeFirst();
+            for (int i = 0; res.next(); i++) {
+                    info[0] = res.getInt(2);
+                    info[1] = res.getInt(3);
+                    info[2] = res.getInt(5);
+                    arr.add(new ReportPage(res.getInt(2), res.getInt(1), res.getBoolean(6), new Date(res.getDate(7)), res.getString(8), res.getString(9), res.getString(10), res.getBoolean(11), res.getBoolean(12), res.getBoolean(13), res.getBoolean(14), res.getString(15), res.getBoolean(16), null));
+                    Report reportholder = new Report(res.getInt(2), res.getInt(3), new Date(res.getDate(4)), res.getInt(5),null, null, null);
+                    if(!reportholder.equals(report.get(report.size())))
+                    report.add(reportholder);
+            }
+            for (Report singlereport : report) {
+                ArrayList<ReportPage> reportpageholder = new ArrayList<>();
+                for (ReportPage reportPage : arr) {
+                    if (reportPage.getReportNr()==singlereport.getReportnr()) {
+                        reportpageholder.add(reportPage);
+                    }
+                }
+                singlereport.setReportPages((ReportPage[])reportpageholder.toArray());
+            }
+            
+
+            con.close();
+
+        } catch (Exception ex) {
+            throw new IllegalArgumentException("not valid reportID");
+        }
+        return report;
+    }
+
+    public int getNumbeOfReportFromDB() {
+
+        int info = 0;
+        ArrayList<ReportPage> arr = new ArrayList<>();
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(Connector.URL, Connector.USERNAME, Connector.PASSWORD);
+            Statement statement = con.createStatement();
+            ResultSet res = statement.executeQuery("SELECT count(*) FROM report;");
+            res.beforeFirst();
+            for (int i = 0; res.next(); i++) {
+                info = res.getInt(1);
+            }
+            con.close();
+
+        } catch (Exception ex) {
+        }
+        return info;
     }
 }
