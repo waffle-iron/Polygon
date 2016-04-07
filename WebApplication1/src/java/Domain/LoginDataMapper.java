6af3ef839e73db9cdca219b22e909de5201f4046
Louise Nielsen <louise.b.nielsen@hotmail.com>
2016-04-07 14:29:05 +0200

    Formatering

--- WebApplication1/src/java/Domain/LoginDataMapper.java
+++ WebApplication1/src/java/Domain/LoginDataMapper.java
@@ -26,9 +26,7 @@ public class LoginDataMapper
                 String username = res.getString("username");
                 String password = res.getString("password");
                 String firmID = res.getString("firmID");
-                System.out.println("test51");
                 String authorization = res.getString("authorization");
-                System.out.println("test52");
                 listOfUsers.add(new Login(username, password, firmID, authorization));
             }
 
