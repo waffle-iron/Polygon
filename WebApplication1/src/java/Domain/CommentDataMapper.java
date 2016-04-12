1ff1f07673b05ca50e026b0c87ab2ea92ffd1d43
EmilUlrikChristoffersen <emilulrikchristoffersen@outlook.dk>
2016-04-12 11:30:33 +0200

    added image upload to database

---
+++ WebApplication1/src/java/Domain/CommentDataMapper.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Domain;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Emil
+ */
+public class CommentDataMapper {
+    public static int getNextCommentNr()
+    {
+        int info = 0;
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection(Connector.URL, Connector.USERNAME, Connector.PASSWORD);
+            Statement statement = con.createStatement();
+            ResultSet res = statement.executeQuery("SELECT max(CommentID) FROM picturelink;");
+            res.next();
+            info = res.getInt(1);
+            con.close();
+
+        } catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        return info;
+    }
+}
